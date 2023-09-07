e740ca600e692efc1ea6ab65b2efd2e1b64f9a25
codify <nurthern2016@gmail.com>
2023-09-08 01:43:28 +0600

    learned basic type, unione type for object & array

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,26 @@
-var character = 'mario';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var chir = function (diametar) {
+    return diametar * Math.PI;
+};
+console.log(chir(7.5));
+var arrays = ['X', 'Y', 'Z'];
+// arrays.push('two')
+// arrays.push(5) //error
+console.log(arrays);
+var mixArray = ['X', 1, true];
+mixArray.push('Y', 2, false);
+console.log(mixArray);
+// unione type with object 
+var obj;
+obj = {
+    types: 'New',
+    format: 231,
+    sell: true,
+};
+console.log(obj);
+var object;
+object = {
+    name: 'Ashraf',
+    age: 41,
+    student: false,
+};
+console.log(object);
--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,47 @@
-const character = 'mario';
 
-console.log(character);
 
-const inputs = document.querySelectorAll('input');
+const chir = (diametar:number) => 
+{
+  return diametar * Math.PI;
+}
 
-inputs.forEach(input => {
-  console.log(input);
-});
+console.log(chir(7.5));
+
+
+let arrays = ['X', 'Y', 'Z'];
+// arrays.push('two')
+// arrays.push(5) //error
+console.log(arrays);
+
+let mixArray = ['X', 1, true];
+mixArray.push('Y', 2, false);
+console.log(mixArray);
+
+
+// unione type with object 
+let obj : object;
+obj = {
+  types : 'New',
+  format : 231,
+  sell : true,
+}
+
+console.log(obj);
+
+
+
+let object : {
+  name : string;
+  age : number;
+  student : boolean;
+}
+
+object = {
+  name : 'Ashraf',
+  age : 41,
+  student : false,
+}
+
+console.log(object);
+
+
