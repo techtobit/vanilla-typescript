964711e7a298244442d866c9a81d531f8ce16cc5
codify <nurthern2016@gmail.com>
2023-09-08 04:41:31 +0600

    access modifier

---
+++ public/accessModifier.js
@@ -0,0 +1,34 @@
+"use strict";
+class myInvoice {
+    // // only can read from anyware but cant write 
+    // readonly client: string;
+    // // cant read or write outside of class 
+    // private details: string;
+    // //(defaul) can read and write from anyware.
+    // public amount: number;
+    // constructor(c: string, d:string, a:number)
+    // {
+    //     this.client = c;
+    //     this.details = d;
+    //     this.amount = a;
+    // }
+    // another way to add access modifier 
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    format() {
+        return `${this.client} owes $${this.amount} for${this.details}`;
+    }
+}
+const invsOne = new myInvoice('mario', 'work on the mario website', 250);
+const invsTwo = new myInvoice('Junior', 'work on the mario-Junior website', 300);
+// console.log(invOne, invTwo);
+// create array base one myInvoice object 
+let myInvoices = [];
+myInvoices.push(invsOne);
+myInvoices.push(invsTwo);
+myInvoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
+});
