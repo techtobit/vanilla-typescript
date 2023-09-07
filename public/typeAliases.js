3dfaf733dba8ec65cc2492f5f2f88901fa5cfea6
codify <nurthern2016@gmail.com>
2023-09-08 02:50:40 +0600

    type Alises

--- public/typeAliases.js
+++ public/typeAliases.js
@@ -2,4 +2,19 @@
 // basicly a it created a group of type.
 // and assing those type to type sttring (stringOrNum)
 // so if have big nested fun then we can use this type string
-// instade of deleclear types everyware 
+// instade of deleclear types everyware
+// basic 
+const logDetails = (uid, item) => {
+    console.log(`${item} has a uid of ${uid}`);
+};
+// -- with dynamic type string 
+const logDetailsPro = (uid, item) => {
+    console.log(`${item} has a uid of ${uid}`);
+};
+// basic obj fun 
+const logDetailsObj = (user) => {
+    console.log(`${user.item} has a uid of ${user.uid}`);
+};
+const logDetailsObjPro = (user) => {
+    console.log(`${user.name} has a uid of ${user.uid}`);
+};
--- src/typeAliases.ts
+++ src/typeAliases.ts
@@ -2,4 +2,28 @@ type stringOrNum = string | number;
 // basicly a it created a group of type.
 // and assing those type to type sttring (stringOrNum)
 // so if have big nested fun then we can use this type string
-// instade of deleclear types everyware 
+// instade of deleclear types everyware
+
+
+
+
+// basic 
+const  logDetails = (uid: string | number, item: string) => {
+    console.log(`${item} has a uid of ${uid}`);
+}
+
+// -- with dynamic type string 
+const  logDetailsPro = (uid: stringOrNum, item: string) => {
+    console.log(`${item} has a uid of ${uid}`);
+}
+// basic obj fun 
+const  logDetailsObj = (user:{uid: string | number, item: string}) => {
+    console.log(`${user.item} has a uid of ${user.uid}`);
+}
+
+// -- with dynamic type string \
+type objectWithName = {name:string, uid: stringOrNum};
+
+const  logDetailsObjPro = (user:objectWithName) => {
+    console.log(`${user.name} has a uid of ${user.uid}`);
+}
